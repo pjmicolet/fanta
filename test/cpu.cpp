86661f3ce1bc14b64b856571ff1c33e3e2b14b52
pjm <p.micolet@gmail.com>
2026-01-31 23:07:56 +0000

    Add basic tests for everything but jmp

--- test/cpu.cpp
+++ test/cpu.cpp
@@ -6,12 +6,48 @@
 TEST_CASE("Basic Adds") {
   using namespace Instructions;
   constexpr auto code = Program<
-    Add<Reg<0>, Reg<0>, Literal<123>>
+    Add<Reg<0>, Reg<0>, Literal<122>>,
+    Add<Reg<0>, Reg<0>, Reg<0>>
   >::load();
 
   CPU cpu{};
   cpu.load_rom(code);
   cpu.run_cycle();
-  REQUIRE_SAME(123, cpu.registers[0]);
+  REQUIRE_SAME(122, cpu.registers[0]);
+  cpu.run_cycle();
+  REQUIRE_SAME(244, cpu.registers[0]);
 }
 
+TEST_CASE("Basic Mov") {
+  using namespace Instructions;
+  constexpr auto code = Program<
+    Mov<Reg<1>, Literal<122>>,
+    Mov<Reg<2>, Reg<1>>
+  >::load();
+
+  CPU cpu{};
+  cpu.load_rom(code);
+  cpu.run_cycle();
+  REQUIRE_SAME(122, cpu.registers[1]);
+  cpu.run_cycle();
+  REQUIRE_SAME(122, cpu.registers[2]);
+}
+
+TEST_CASE("Basic Sub") {
+  using namespace Instructions;
+  constexpr auto code = Program<
+    Mov<Reg<0>, Literal<122>>,
+    Mov<Reg<2>, Literal<121>>,
+    Sub<Reg<1>, Reg<0>, Reg<2>>,
+    Sub<Reg<2>, Reg<1>, Literal<1>>
+  >::load();
+
+  CPU cpu{};
+  cpu.load_rom(code);
+  cpu.run_cycle();
+  cpu.run_cycle();
+  cpu.run_cycle();
+  REQUIRE_SAME(1, cpu.registers[1]);
+  cpu.run_cycle();
+  REQUIRE_SAME(0, cpu.registers[2]);
+}
