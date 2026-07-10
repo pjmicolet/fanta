553983b31bcc4b7e11a0ef3a884b9bc31b4cf4a7
pjm <p.micolet@gmail.com>
2026-07-10 19:26:44 +0100

    Add return to testing
    Before ctest would fail silently, now it wont

--- common/testframework/testing.hpp
+++ common/testframework/testing.hpp
@@ -1,60 +1,61 @@
 #pragma once
-#include <string>
-#include <vector>
-#include <memory>
-#include <iostream>
 #include "assertions.hpp"
 #include "result_format.hpp"
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 
 #define FUNC_NAME TestCase
-#define GEN_CLASS_NAME3(name, line) name ## line
+#define GEN_CLASS_NAME3(name, line) name##line
 #define GEN_CLASS_NAME2(name, line) GEN_CLASS_NAME3(name, line)
 #define GEN_CLASS_NAME(name) GEN_CLASS_NAME2(name, __COUNTER__)
 
-#define GEN_INSTANCE(name) name ## Instance
+#define GEN_INSTANCE(name) name##Instance
 
 struct TestCase;
 
 struct TestCase {
-    TestCase(std::string name) : name_(name) {
-    };
-    virtual auto run() -> void = 0;
-    auto report() {
-        std::cout << "Running " << name_ << "...\n";
-        run();
-    };
-    const std::string name_;
+  TestCase(std::string name) : name_(name) {};
+  virtual auto run() -> void = 0;
+  auto report() {
+    std::cout << "Running " << name_ << "...\n";
+    run();
+  };
+  const std::string name_;
 };
 
-static std::vector<TestCase*> cases;
+static std::vector<TestCase *> cases;
 
+#define GEN_TEST(FUNCNAME, strdescriptor, ...)                                 \
+  struct FUNCNAME : public TestCase {                                          \
+    FUNCNAME() : TestCase(strdescriptor) { cases.push_back(this); };           \
+    auto run() -> void override;                                               \
+  };                                                                           \
+  static FUNCNAME GEN_INSTANCE(FUNCNAME){};                                    \
+  void FUNCNAME::run()
 
-#define GEN_TEST(FUNCNAME, strdescriptor,...)\
-struct FUNCNAME : public TestCase {\
-    FUNCNAME() : TestCase(strdescriptor) {\
-        cases.push_back(this);\
-    };\
-    auto run() -> void override;\
-};\
-static FUNCNAME GEN_INSTANCE(FUNCNAME){};\
-void FUNCNAME::run()\
-
-#define TEST_CASE(name) GEN_TEST(GEN_CLASS_NAME(FUNC_NAME),name)
+#define TEST_CASE(name) GEN_TEST(GEN_CLASS_NAME(FUNC_NAME), name)
 
 int main() {
-    std::vector<std::unique_ptr<Test::Format::Result>> results;
-    for(const auto& tCase : cases) {
-        try {
-            tCase->report();
-        } catch(const TestException& e ) {
-            results.emplace_back(static_cast<std::unique_ptr<Test::Format::Result>>(std::make_unique<Test::Format::Failed>(tCase->name_, e.what())));
-            continue;
-        }
-        results.emplace_back(static_cast<std::unique_ptr<Test::Format::Result>>(std::make_unique<Test::Format::Passed>(tCase->name_)));
+  std::vector<std::unique_ptr<Test::Format::Result>> results;
+  bool __failed = 0;
+  for (const auto &tCase : cases) {
+    try {
+      tCase->report();
+    } catch (const TestException &e) {
+      __failed = 1;
+      results.emplace_back(static_cast<std::unique_ptr<Test::Format::Result>>(
+          std::make_unique<Test::Format::Failed>(tCase->name_, e.what())));
+      continue;
     }
+    results.emplace_back(static_cast<std::unique_ptr<Test::Format::Result>>(
+        std::make_unique<Test::Format::Passed>(tCase->name_)));
+  }
 
-    const std::string indent = "    ";
-    for(const auto& result : results) {
-        std::cout << indent << result->to_string() << "\n";
-    }
+  const std::string indent = "    ";
+  for (const auto &result : results) {
+    std::cout << indent << result->to_string() << "\n";
+  }
+  return __failed;
 }
